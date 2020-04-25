7c460c41cb4f309e843b4cb5b167617459f5667f
iankhor <iankhorbc.thecoder@gmail.com>
2020-04-25 15:05:51 +1000

    replace atl with ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         include: path.resolve(__dirname, 'src'),
-        use: 'awesome-typescript-loader',
+        use: 'ts-loader',
       },
       {
         test: /\.css$/i,
